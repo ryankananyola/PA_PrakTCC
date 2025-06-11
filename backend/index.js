7d5f2f49c3803f6ebbe8004a73fe94b024c7ea37
ryankananyola <mariadestan2027@gmail.com>
2025-06-11 18:47:17 +0700

    update index js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ import paymentRoute from "./routes/paymentRoute.js";
 const app = express();
 
 const corsOptions = {
-  origin: ['http://127.0.0.1:5500', 'http://localhost:5500', 'http://localhost:3000'],
+  origin: ['http://127.0.0.1:5500', 'http://localhost:5500', 'http://localhost:3000', 'https://laundryrr-dot-g-08-450802.uc.r.appspot.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
